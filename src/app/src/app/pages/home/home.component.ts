875a877fd4d35c68ff814ca92425e6cf69509d61
garethduncandev <garethduncandev@outlook.com>
2024-01-02 16:32:45 +0000

    animation

--- src/app/src/app/pages/home/home.component.ts
+++ src/app/src/app/pages/home/home.component.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
+import { InViewportModule } from 'ng-in-viewport';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [InViewportModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  public image1IsInViewport = false;
+  public image2IsInViewport = false;
+  public image3IsInViewport = false;
+  public text1IsInViewport = false;
+  public form1IsInViewport = false;
+
   public scroll(el: HTMLElement): void {
     el.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  public alert(): void {
+    alert('You are in viewport!');
+  }
+
+  public onIntersection({ visible }: { target: Element; visible: boolean }, image: string): void {
+    if (image === '1') {
+      this.image1IsInViewport = visible;
+    }
+
+    if (image === '2') {
+      this.image2IsInViewport = visible;
+    }
+
+    if (image === '3') {
+      this.image3IsInViewport = visible;
+    }
+
+    if (image === 'text') {
+      this.text1IsInViewport = visible;
+    }
+
+    if (image === 'form') {
+      this.form1IsInViewport = visible;
+    }
   }
 }
