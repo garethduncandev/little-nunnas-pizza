3a73d1707dd8aa982c81e1743a427feaac051c35
garethduncandev <garethduncandev@outlook.com>
2024-01-07 17:12:58 +0000

    pizza border

---
+++ src/app/src/app/components/pizza-border/pizza-border.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PizzaBorderComponent } from './pizza-border.component';
+
+describe('PizzaBorderComponent', () => {
+  let component: PizzaBorderComponent;
+  let fixture: ComponentFixture<PizzaBorderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PizzaBorderComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(PizzaBorderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/src/app/components/pizza-border/pizza-border.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-border',
+  standalone: true,
+  imports: [],
+  templateUrl: './pizza-border.component.html',
+  styleUrl: './pizza-border.component.scss'
+})
+export class PizzaBorderComponent {
+
+}
