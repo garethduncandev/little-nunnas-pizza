b9b139759a942536b8c5502e9fe5d14e1ca3b9ff
garethduncandev <garethduncandev@outlook.com>
2024-02-11 18:45:25 +0000

    fix

--- src/app/src/app/appsettings/app-settings.factory.ts
+++ src/app/src/app/appsettings/app-settings.factory.ts
@@ -5,7 +5,6 @@ import { Observable, of } from 'rxjs';
 import { catchError, mergeMap, tap } from 'rxjs/operators';
 import { AppSettingsService } from './app-settings.service';
 import { AppSettings } from './appsettings';
-import { DiagnosticsClient } from '../web-api-client';
 
 export function appSettingsFactory(): () => Observable<void> {
   const location = inject(Location);
@@ -17,12 +16,6 @@ export function appSettingsFactory(): () => Observable<void> {
 
   const appSettingsPath = `assets/appsettings.json?v=${buildNumber}`;
   const fullAppSettingsPath = location.prepareExternalUrl(appSettingsPath);
-
-  inject(DiagnosticsClient)
-    .getDiagnostics()
-    .subscribe((response) => {
-      console.log(response);
-    });
 
   const request = (): Observable<void> =>
     httpClient
