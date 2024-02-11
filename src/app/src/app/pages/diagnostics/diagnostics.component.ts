d5b2ea037e413b6951525f781157f07313d5bb8b
garethduncandev <garethduncandev@outlook.com>
2024-02-11 17:42:43 +0000

    Added diagnostics endpoint

--- src/app/src/app/pages/diagnostics/diagnostics.component.ts
+++ src/app/src/app/pages/diagnostics/diagnostics.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { DiagnosticsClient } from '../../web-api-client';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-diagnostics',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './diagnostics.component.html',
   styleUrl: './diagnostics.component.scss',
 })
-export class DiagnosticsComponent {}
+export class DiagnosticsComponent implements OnInit {
+  public diagnostics = signal<Date | undefined>(undefined);
+
+  public constructor(private diagnosticsClient: DiagnosticsClient) {}
+
+  public ngOnInit(): void {
+    this.diagnosticsClient.getDiagnostics().subscribe((response) => {
+      this.diagnostics.set(response);
+      console.log('Diagnostics page', response);
+    });
+  }
+}
