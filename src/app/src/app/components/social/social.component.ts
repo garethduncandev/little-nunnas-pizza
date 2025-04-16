bb7f68293614267ca449f52c50bc5f3ca1f2611d
garethduncandev <2130124+garethduncandev@users.noreply.github.com>
2025-04-16 19:58:53 +0100

    tailwind 4

--- src/app/src/app/components/social/social.component.ts
+++ src/app/src/app/components/social/social.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { FacebookProfileUrl, InstagramProfileUrl } from '../../consts';
 
 @Component({
-    selector: 'app-social',
-    imports: [],
-    templateUrl: './social.component.html',
-    styleUrl: './social.component.scss'
+  selector: 'app-social',
+  imports: [],
+  templateUrl: './social.component.html',
+  styleUrl: './social.component.scss',
 })
 export class SocialComponent {
   public facebookProfileUrl = FacebookProfileUrl;
